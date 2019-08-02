e324b39d02cad40b26b2c4a541d6ad49894fd570
Paulo Cesar Morandi Massuci <pmassuci@ciandt.com>
2019-08-02 12:13:48 -0300

    commit inicial

---
+++ MusicList/MusicList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicList
+//
+//  Created by Paulo Cesar Morandi Massuci on 02/08/19.
+//  Copyright © 2019 Paulo Cesar Morandi Massuci. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
